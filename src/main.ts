51b96426c42d5d73ced75d7fa46b15c2fe0aa3e8
Daniel Waxweiler <daniel.waxweiler@list.lu>
2022-04-21 09:49:14 +0000

    increase payload limit to 1mb

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
+import * as bodyParser from 'body-parser'
+
+const PAYLOAD_LIMIT = '1mb'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,6 +16,8 @@ async function bootstrap() {
   app.getHttpAdapter().getInstance().disable('x-powered-by')
   const configService = app.get(ConfigService)
   const port = configService.get<number>('port')
+  app.use(bodyParser.json({ limit: PAYLOAD_LIMIT }))
+  app.use(bodyParser.urlencoded({ limit: PAYLOAD_LIMIT }))
   await app.listen(port)
 }
 bootstrap()
