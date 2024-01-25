09280a8d4923f089c3a51d6d5e8b0e8954e6613f
Adriano Gama <adriano.gama@list.lu>
2024-01-25 17:47:19 +0000

    Scripts for battery monitoring app4cam-frontend#9

---
+++ scripts/variscite/battery-monitoring/main.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+    char *filename = "/dev/i2c-2";
+    int file, addr = 0x4d;
+    char buf[2];
+    float raw, voltage;
+
+    if ((file = open(filename, O_RDWR)) < 0) {
+        perror("Failed to open the i2c bus");
+        return -1;
+    }
+
+    if (ioctl(file, I2C_SLAVE, addr) < 0) {
+        perror("Failed to acquire bus access and/or talk to slave.\n");
+        return -1;
+    }
+
+    if (read(file, buf, 2) != 2) {
+        perror("Failed to read 2 bytes from the i2c bus.\n");
+        return -1;
+    }
+    else
+    {
+        raw = (float)((buf[0] & 0b00001111)<<8)+buf[1]; // 10 bits
+        voltage = 0.003539425 * raw + 0.211151;
+
+        printf("%.1f", voltage);
+    }
+    return 1;
+}
